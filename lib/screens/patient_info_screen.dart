1eab9af5530b17d082fa17c465b4e7a70a58788f
anand <anandsingh810433@gmail.com>
2025-03-28 12:01:24 +0530

    'pasten_scr'

--- lib/screens/patient_info_screen.dart
+++ lib/screens/patient_info_screen.dart
@@ -100,7 +100,7 @@ class _PatientInfoScreenState extends State<PatientInfoScreen>
             child: Column(
               children: [
                 SizedBox(
-                  height: 80,
+                  height: 0,
                 ),
                 // Main Form
                 ScaleTransition(
